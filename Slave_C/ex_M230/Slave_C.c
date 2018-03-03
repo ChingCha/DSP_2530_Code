fd315e31092e644cd9ce8e7d04a0cb14c671d653
ChingCha <34024702+ChingCha@users.noreply.github.com>
2018-03-03 21:20:24 +0800

    20180303-03
    20180303-03

--- Slave_C/ex_M230/Slave_C.c
+++ Slave_C/ex_M230/Slave_C.c
@@ -1,4 +1,9 @@
-//ZigBee_PRO
+/*------------------------------------------------------------------
+File name: Slave_B.c
+Description:This is I.O.L system master control application functions.
+Updated:2018/03/03
+-------------------------------------------------------------------*/
+//-----------------------CC2530 Include--------------------------
 #include "hal_defs.h"
 #include "hal_mcu.h"
 #include "hal_board.h"
@@ -11,25 +16,17 @@
 #include "hal_keypad.h"
 #include "Program.h"
 #include "hal_lcd.h"
-
-//C_Compiler
 #include <string.h>
-
-// Application parameters
+//---------------------------Define-----------------------------------
 #define RF_CHANNEL                18      // 2.4 GHz RF channel
-
 // BasicRF address definitions
 #define PAN_ID                	0x1111
 #define C_ZONE           		0x2244
 #define APP_PAYLOAD_LENGTH        255
 #define Master            	0x3333
-
-
-//---------------------------------------------------------------
 // Application states
 #define IDLE                      0
 #define SEND_CMD                  1
-
 #ifdef SECURITY_CCM
     // Security key
     static uint8 key[] = 
@@ -37,37 +34,28 @@
         0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 
     };
 #endif
-//-------------------------------------------------
+//-------------------------------------------------------------------
 static uint8 pRxData[APP_PAYLOAD_LENGTH];
 static uint8 pTxData[APP_PAYLOAD_LENGTH];
 static basicRfCfg_t basicRfConfig;
-//-------------------------------------------------
+//-------------------------variable----------------------------------
 uint8 key;
 uint16 ProgramDelay;
-//-------------------------------------------------
+//-------------------------Function---------------------------------
 void SlaveInit(void);
 void Program(uint8 b);
 void Mode(uint8 a);
 void ShowLCD(void);
 uint8 BreakMode(uint8 i,uint8 j);
 void SendData(uint8 Program,uint8 RxData);
-/********************************
-主程式
-********************************/
+//-------------------------main function----------------------------------
 void main(void)
 {   
 	SlaveInit();
     while (1)
-	{		
-        while (!basicRfPacketIsReady())
-		{
-            halLedToggle(7);
-            halMcuWaitMs(10);
-			halLedToggle(7);
-            halMcuWaitMs(10);
-        }				
+	{				
 		while(basicRfReceive(pRxData, APP_PAYLOAD_LENGTH, NULL) > 0)
-		{						
+		{									
 			Mode(pRxData[0]);
 		}   
     }
@@ -81,13 +69,12 @@ void SlaveInit(void)
 		basicRfConfig.securityKey = key;
 	#endif 
 	halBoardInit();	
-	halLedSet(8);
 	basicRfConfig.myAddr = C_ZONE;
     if (basicRfInit(&basicRfConfig) == FAILED){}	
     basicRfReceiveOn();			
 	ProgramDelay = 300;
     halLcdWriteString(HAL_LCD_LINE_1,0,"I.O.L_System");
-	halLcdWriteString(HAL_LCD_LINE_2,0,"Slave_C Ready..");
+	halLcdWriteString(HAL_LCD_LINE_2,0,"SlaveC Ready...");
 }
 void Mode(uint8 a)
 {
@@ -97,15 +84,15 @@ void Mode(uint8 a)
 		case 1:
 			ShowLCD();
 			halLcdWriteString(HAL_LCD_LINE_1,12,"1");
-			for(uint8 i = 0;i < 100;i++)
+			for(int i = 0x0001;i < 0x0064;i++)
 			{
 				READProgram(i);
 				SendData(i,pRxData[10]);
 				halLcdDisplayUint8(HAL_LCD_LINE_2,8,HAL_LCD_RADIX_DEC,i);
-				for(uint8 j = 0;j < 8;j++)
+				for(uint8 j = 0x0000;j < 0x0008;j++)
 				{
 					LedProgram(j);
-					halMcuWaitMs(250);
+					halMcuWaitMs(ProgramDelay);
 				}				
 				halLedSetPort(0x00);
 				halMcuWaitMs(ProgramDelay);
@@ -115,7 +102,7 @@ void Mode(uint8 a)
 		case 2:
 			ShowLCD();
 			halLcdWriteString(HAL_LCD_LINE_1,12,"2");
-			for(int i = 0;i < 8;i++)
+			for(int i = 0x0000;i < 0x000A;i++)
 			{
 				READProgram(pRxData[i+2]);
 				SendData(pRxData[i+2],pRxData[10]);
@@ -123,7 +110,7 @@ void Mode(uint8 a)
 				for(int j = 0;j < 8;j++)
 				{
 					LedProgram(j);
-					halMcuWaitMs(250);
+					halMcuWaitMs(ProgramDelay);
 				}
 				i = BreakMode(i,2);
 				halMcuWaitMs(ProgramDelay);
@@ -156,7 +143,7 @@ uint8 BreakMode(uint8 i,uint8 j)
 }
 void SendData(uint8 Program,uint8 RxData)
 {
-		pTxData[0] = 0x2244;
+		pTxData[0] = 0x01;
 		pTxData[1] = Program;
 		basicRfSendPacket(Master,pTxData,APP_PAYLOAD_LENGTH);
 }
